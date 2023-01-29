e3e5676f00985d3431639249f117d46bbe6d21a5
Fran Cazorla <fcmeca@gmail.com>
2023-01-29 13:13:51 +0100

    Create project

---
+++ ProductsGNB/ProductsGNB/ProductsGNBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductsGNBApp.swift
+//  ProductsGNB
+//
+//  Created by Fran Cazorla on 29/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ProductsGNBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
